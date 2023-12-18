5fe377b5a29d7bb3db76594432a893cf4c35e627
shahbazcse <cseshahbaz@gmail.com>
2023-12-18 19:53:12 +0530

    Added NavBar and Sidebar

--- lumos/app/(dashboard)/(routes)/dashboard/page.tsx
+++ lumos/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
 
 const DashbordPage = () => {
   return (
     <div>
       <p>Dashbord Page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
---
+++ lumos/components/SideBar-Mobile.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "@/components/SideBar";
+
+const SideBarMobile = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <SideBar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default SideBarMobile;
