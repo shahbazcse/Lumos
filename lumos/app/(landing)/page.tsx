692db8df195e5f9c8ee109b64ef9e785e79e67c3
shahbazcse <cseshahbaz@gmail.com>
2023-12-24 18:49:37 +0530

    Added Landing Hero

--- lumos/app/(landing)/page.tsx
+++ lumos/app/(landing)/page.tsx
@@ -1,10 +1,14 @@
+import LandingContent from "@/components/Landing-Content";
+import LandingHero from "@/components/Landing-Hero";
 import LandingNavbar from "@/components/Landing-NavBar";
 import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
-      <LandingNavbar/>
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent/>
     </div>
   );
 };
