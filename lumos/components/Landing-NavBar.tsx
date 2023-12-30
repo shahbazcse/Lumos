cc9e4ff24129690598a21b0d8b83630adfe885ec
shahbazcse <cseshahbaz@gmail.com>
2023-12-30 19:56:35 +0530

    Refactored Code

--- lumos/components/Landing-NavBar.tsx
+++ lumos/components/Landing-NavBar.tsx
@@ -36,10 +36,10 @@ const LandingNavbar = () => {
     <nav className="px-4 md:px-12 py-6 bg-transparent flex items-center justify-between text-white border-b-[1px] border-gray-800">
       <div>
         <Link href="/" className="flex items-center">
-          <div className="relative h-8 w-8 mr-3">
+          <div className="relative h-5 w-5 md:h-8 md:w-8 mr-2.5">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className={cn("text-md md:text-2xl font-bold text-white", font.className)}>
+          <h1 className={cn("text-md md:text-2xl font-bold text-white tracking-wide", font.className)}>
             Lumos AI
           </h1>
         </Link>
