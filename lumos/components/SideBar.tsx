bb7bc0c3fbeb0e377ccb40e361ee740f8219a976
shahbazcse <cseshahbaz@gmail.com>
2024-01-03 18:56:53 +0530

    Refactored Code

--- lumos/components/SideBar.tsx
+++ lumos/components/SideBar.tsx
@@ -72,7 +72,7 @@ const SideBar = () => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative w-8 h-8 mr-2">
+          <div className="relative w-7 h-7 mr-3">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
