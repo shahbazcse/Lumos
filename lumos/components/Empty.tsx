aeb85383328f12121cb3e749e658d71bebb08db1
shahbazcse <cseshahbaz@gmail.com>
2024-01-07 17:53:26 +0530

    Changed Chatbot Avatar & Empty Image

--- lumos/components/Empty.tsx
+++ lumos/components/Empty.tsx
@@ -7,9 +7,9 @@ interface EmptyProps {
 
 export const Empty = ({ label }: EmptyProps) => {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center">
-      <div className="relative h-72 w-72">
-        <Image alt="Empty" fill src={"/empty.png"} />
+    <div className="h-full p-20 flex flex-col items-center justify-center gap-6">
+      <div className="relative h-32 w-32 opacity-[70%]">
+        <Image alt="Empty" fill src={"/chatbot.png"} />
       </div>
       <p className="text-muted-foreground text-sm text-center">{label}</p>
     </div>
