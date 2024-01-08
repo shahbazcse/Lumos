e9533f94fd46d2ed9df5acf43eb90b38a4dd4c1b
shahbazcse <cseshahbaz@gmail.com>
2024-01-08 12:56:33 +0530

    Refactored Code

--- lumos/app/(auth)/layout.tsx
+++ lumos/app/(auth)/layout.tsx
@@ -11,12 +11,17 @@ const montserrat = Montserrat({
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex items-center justify-center h-full bg-[#000000]">
-      <div className="flex items-center justify-center gap-1.5 relative mb-[39rem] sm:mb-[37rem]">
+      <div className="flex items-center justify-center gap-1.5 relative mb-[44rem]">
         <div className="relative w-8 h-8 mr-3">
           <Image fill alt="Logo" src="/logo.png" />
         </div>
-        <h1 className={cn("text-3xl text-white font-bold", montserrat.className)}>
-          Lumos AI
+        <h1
+          className={cn("text-4xl text-white font-bold", montserrat.className)}
+        >
+          Lumos{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400">
+            AI
+          </span>
         </h1>
       </div>
       <div className="absolute">{children}</div>
