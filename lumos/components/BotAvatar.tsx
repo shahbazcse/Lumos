808fd9f24edc4ea30a7c380b36f81105b6627c4c
shahbazcse <cseshahbaz@gmail.com>
2024-01-10 21:07:47 +0530

    Refactored Code

--- lumos/components/BotAvatar.tsx
+++ lumos/components/BotAvatar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
-export const BotAvatar = ({ avatar }) => {
+export const BotAvatar = ({ avatar }: any) => {
   return (
     <Avatar>
       <AvatarImage className="p-1" src={avatar} />
