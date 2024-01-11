fae367d4a5970dd16c235145181a29d399579366
shahbazcse <cseshahbaz@gmail.com>
2024-01-11 20:04:08 +0530

    Added Custom AI Prompts

--- lumos/app/api/code/route.ts
+++ lumos/app/api/code/route.ts
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 const instructionMessage: ChatCompletionMessageParam = {
   role: "system",
   content:
-    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations. Always give separate explainations for code. Mention the coding language before the code.",
 };
 
 export async function POST(req: Request) {
