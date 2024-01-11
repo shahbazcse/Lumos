3ec8afda2b342c4691b01690d749c12bfd38a1df
shahbazcse <cseshahbaz@gmail.com>
2024-01-11 20:36:36 +0530

    Refactored Code

--- lumos/app/api/code/route.ts
+++ lumos/app/api/code/route.ts
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 const instructionMessage: ChatCompletionMessageParam = {
   role: "system",
   content:
-    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations. Always give separate explainations for code. Mention the coding language before the code.",
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations. Always give separate explanations for code.",
 };
 
 export async function POST(req: Request) {
