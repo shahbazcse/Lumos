0f3cefc34e497d08d82ba29c6dfe49c4c1b0471b
shahbazcse <cseshahbaz@gmail.com>
2024-01-17 21:08:41 +0530

    Added Image Provider BASEURL in environment file

--- lumos/next.config.js
+++ lumos/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+                hostname: `${process.env.IMAGE_PROVIDER_BASEURL}`,
                 port: '',
             }
         ]
