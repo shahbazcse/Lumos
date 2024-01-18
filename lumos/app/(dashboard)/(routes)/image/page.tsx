134084a9686f86f113d2223d1c29b3dd09952593
shahbazcse <cseshahbaz@gmail.com>
2024-01-18 20:25:48 +0530

    Changed Empty Image

--- lumos/app/(dashboard)/(routes)/image/page.tsx
+++ lumos/app/(dashboard)/(routes)/image/page.tsx
@@ -159,7 +159,7 @@ const ImagePage: any = () => {
             </div>
           )}
           {images.length === 0 && !isLoading && (
-            <Empty image={"/chatbot.png"} label="Generate Images Now!" />
+            <Empty image={"/image.png"} label="Generate Images Now!" />
           )}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
             {images.map((src) => (
