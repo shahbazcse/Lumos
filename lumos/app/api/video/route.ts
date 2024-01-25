de2ec1092686982782ac9df04d34115944c5bdf4
shahbazcse <cseshahbaz@gmail.com>
2024-01-25 21:24:47 +0530

    TODO

--- lumos/app/api/video/route.ts
+++ lumos/app/api/video/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
-
+    // API taking more than 10s, move this API to firebase
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
