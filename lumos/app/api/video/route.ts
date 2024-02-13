d47fd66e31ac416d11d2a614761cb1941d4c3c1c
shahbazcse <cseshahbaz@gmail.com>
2024-02-13 19:34:58 +0530

    Added API Limit to all APIs

--- lumos/app/api/video/route.ts
+++ lumos/app/api/video/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
@@ -19,6 +21,13 @@ export async function POST(req: Request) {
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
+
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired", { status: 403 });
+    }
+
     // API taking more than 10s, move this API to firebase
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
@@ -28,6 +37,8 @@ export async function POST(req: Request) {
         },
       }
     );
+
+    await increaseApiLimit();
 
     return NextResponse.json(response);
   } catch (error) {
