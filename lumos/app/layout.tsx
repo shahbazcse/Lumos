960a3510ccd2511f4c6990931742927e3429cc2c
shahbazcse <cseshahbaz@gmail.com>
2024-02-18 22:55:27 +0530

    Added Toast Notifications in App Layout

--- lumos/app/layout.tsx
+++ lumos/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import ModalProvider from "@/components/ModalProvider";
+import ToasterProvider from "@/components/ToasterProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
+          <ToasterProvider />
           {children}
         </body>
       </html>
