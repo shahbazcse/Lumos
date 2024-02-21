4149a80e8ad2cb0806a76be68afea8e3e766d939
shahbazcse <cseshahbaz@gmail.com>
2024-02-21 22:45:01 +0530

    Refactored Code

--- lumos/components/CrispChat.tsx
+++ lumos/components/CrispChat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure(`${process.env.CRISP_ID}`);
+    Crisp.configure("fd436b8e-de78-4041-a379-d12e83e56a59");
   }, []);
   return null;
 };
