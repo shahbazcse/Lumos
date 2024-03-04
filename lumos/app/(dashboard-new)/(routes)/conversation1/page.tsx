884acd6410cb225a27d4b8f292cb3134b9c98c1f
shahbazcse <cseshahbaz@gmail.com>
2024-03-04 20:52:03 +0530

    Setup New Dashboard Layout

---
+++ lumos/app/(dashboard-new)/(routes)/conversation1/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConversationPage = () => {
+  return <div>NewConversationPage</div>;
+};
+
+export default ConversationPage;
---
+++ lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return <div>NewDashboardPage</div>;
+};
+
+export default DashboardPage;
