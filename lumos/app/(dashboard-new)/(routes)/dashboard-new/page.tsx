b799a57bcf3c46a2f140fae132b9e7d9fd10236a
shahbazcse <cseshahbaz@gmail.com>
2024-03-21 23:04:09 +0530

    Fixed Layout

--- lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
+++ lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
@@ -7,15 +7,17 @@ const DashboardPage = () => {
     <div className="grid grid-cols-8">
       <div className="col-span-5 px-6 pt-8 pb-4 space-y-8">
         <p className="text-3xl font-semibold">Explore latest AI models</p>
-        <ScrollArea className="h-[45rem]">
-          <div className="grid grid-rows-1 gap-6 pr-5">
-            {[...Array(10)].map((_, index) => (
-              <Card key={index} className="w-full h-36">
-                <CardContent></CardContent>
-              </Card>
-            ))}
-          </div>
-        </ScrollArea>
+        <div className="bg-indigo-950 bg-opacity-50 rounded-md px-2 py-3">
+          <ScrollArea className="h-[43.5rem] px-2 py-2">
+            <div className="grid grid-rows-1 gap-5 px-2">
+              {[...Array(10)].map((_, index) => (
+                <Card key={index} className="w-full h-[9.68rem] border-none">
+                  <CardContent></CardContent>
+                </Card>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
       </div>
       <div className="col-span-3">World</div>
     </div>
