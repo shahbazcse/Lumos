81e3c6a6cdb17ee93ab2543ecfada055aa1a2609
shahbazcse <cseshahbaz@gmail.com>
2024-03-21 22:31:29 +0530

    Refactored

--- lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
+++ lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import React from "react";
 
