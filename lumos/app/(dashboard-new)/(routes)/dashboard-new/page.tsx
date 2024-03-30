9c242529422206bdc7ef7efe025f72131b3d2cf9
shahbazcse <cseshahbaz@gmail.com>
2024-03-30 17:37:08 +0530

    Improved Layout

--- lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
+++ lumos/app/(dashboard-new)/(routes)/dashboard-new/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
@@ -11,34 +12,39 @@ import React from "react";
 
 const DashboardPage = () => {
   return (
-    <div className="relative grid grid-cols-8 p-8 items-start gap-6">
-      <div className="col-span-5 space-y-7">
-        <p className="text-2xl font-semibold px-6 py-2.5 rounded-2xl w-fit bg-gray-950 bg-opacity-40 border border-slate-800 sticky z-10 top-28">
+    <div className="grid grid-cols-8 px-6 py-8 items-start gap-6 h-full">
+      <div className="col-span-5 space-y-5">
+        <p className="text-2xl backdrop-blur-lg py-2 px-6 font-semibold rounded-full w-fit sticky z-10 top-32">
           Explore latest AI models
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-4">
-          {[...Array(10)].map((_, index) => (
-            <Card key={index} className="aspect-square border-none rounded-xl">
-              <CardContent></CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
-      <div className="col-span-3 space-y-8 sticky z-10 top-28">
-        <p className="text-lg font-semibold px-6 py-2.5 rounded-2xl w-fit bg-gray-950 bg-opacity-40 border border-slate-800">
-          Recent Generations
-        </p>
-        <ScrollArea className="h-[44rem]">
-          <Table className="bg-indigo-950 rounded-md bg-opacity-50 h-[9.68rem]">
-            <TableBody>
-              {[...Array(20)].map((item, index) => (
-                <TableRow key={0} className="h-20">
-                  <TableCell className="font-medium">Hello</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+        <ScrollArea>
+          <div className="grid grid-rows-1 gap-5 px-4 scroll-smooth transition-all">
+            {[...Array(20)].map((_, index) => (
+              <Card key={index} className="border-none rounded-xl h-[14.1rem]">
+                <CardContent></CardContent>
+              </Card>
+            ))}
+          </div>
         </ScrollArea>
+      </div>
+      <div className="col-span-3 space-y-7 sticky z-10 top-32">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold rounded-2xl w-fit">
+            Recent Generations
+          </p>
+          <Button className="rounded-full" variant="outline">
+            View all
+          </Button>
+        </div>
+        <Table className="bg-indigo-950 rounded-md bg-opacity-50 h-[9.68rem]">
+          <TableBody>
+            {[...Array(7)].map((item, index) => (
+              <TableRow key={0} className="h-24">
+                <TableCell className="font-medium">Hello</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
